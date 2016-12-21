26fbc925c681b2961607f1bc23d964f75bb73605
birusblue <birusblue@gmail.com>
2016-12-21 09:09:09 +0530

    fib added

--- src/exercisePackage/MainClass.java
+++ src/exercisePackage/MainClass.java
@@ -4,8 +4,19 @@ public class MainClass
 {
 	public static void main(String[] args)
 	{
+		System.out.println(fibonacci(5));
 		
 	}
-	
+	public static int fibonacci(int n)
+	{
+		if(n==1)
+		{ return 1;}
+		else if(n==0)
+		{return 0;}
+		else 
+		{ return fibonacci(n-1)+ fibonacci(n-2) ; }
+		
+		
+	}
 	
 }
