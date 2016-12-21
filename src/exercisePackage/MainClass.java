4be5434397cc0382f9f637646d1383446ed11272
birusblue <birusblue@gmail.com>
2016-12-21 09:29:38 +0530

    fibprint

--- src/exercisePackage/MainClass.java
+++ src/exercisePackage/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass
 	public static void main(String[] args)
 	{
 		System.out.println(fibonacci(5));
+		fibonacciPrint(5);
 		
 	}
 	public static int fibonacci(int n)
@@ -15,8 +16,20 @@ public class MainClass
 		{return 0;}
 		else 
 		{ return fibonacci(n-1)+ fibonacci(n-2) ; }
-		
-		
+	}
+	 
+	public static void fibonacciPrint(int n)
+	{
+		int a,b,c;
+		a=0;
+		b=1;
+		for(int i=0;i<n;i++)
+		{
+			c=a;
+			System.out.println(a);
+			a=b;
+			b=c+b;
+		}
 	}
 	
 }
